c0e8364001f869ff062e55f19a84203ecbc63147
Javifdz12 <101470100+Javifdz12@users.noreply.github.com>
2023-03-29 16:42:45 +0200

    gg

--- barbero.py
+++ barbero.py
@@ -3,15 +3,10 @@ import time
 
 class Barbero(threading.Thread):
     silla=threading.Lock()
-    def __init__(self,cola):
+    def __init__(self):
         threading.Thread.__init__(self)
-        if cola==[]:
-            self.dormido=True
-            print('estoy dormido')
-        else:
-            self.dormido=False
-            print('estoy despierto')
-        self.cola=cola
+        self.cola=[]
+        self.dormido=True
 
     def atender(self):
         if self.cola!=[] and self.dormido!=True:
@@ -19,13 +14,14 @@ class Barbero(threading.Thread):
                 print(f'Adelante {cliente.nombre}')
                 Barbero.silla.acquire()
                 print(f'Silla ocupada por {cliente.nombre}')
-                time.sleep(2)
+                time.sleep(5)
                 Barbero.silla.release()
                 print(f'{cliente.nombre} ya se ha cortado el pelo')
                 print('Silla desocupada')
                 self.cola.remove(cliente)
         else:
             self.dormido=True
+            print('Toca siesta')
 
     def run(self):
         while True:
